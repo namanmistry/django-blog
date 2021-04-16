37d769b00c523383017ffc9f42d34ec12f4b7b6e
namanmistry <65106237+namanmistry@users.noreply.github.com>
2021-04-16 14:04:10 +0530

    final

---
+++ users/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm,UserUpdateForm
+def register(request):
+    if request.method=="POST":
+        form=UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username=form.cleaned_data.get('username')
+            messages.success(request,f'Account for {username} created.')
+            return redirect('blog-home')
+    else:
+        form=UserRegisterForm()
+    return render(request,'users/register.html',{'form':form})
+
+@login_required
+def profile(request):
+
+    if request.method=="POST":
+        u_form=UserUpdateForm(request.POST,instance=request.user)
+        
+
+        if u_form.is_valid():
+            u_form.save()
+            messages.success(request,f'your account has been updated!.')
+            return redirect('profile')
+    else:
+        u_form=UserUpdateForm(instance=request.user)
+    
+    context={
+        'u_form':u_form,
+    }
+    return render(request,'users/profile.html',context)
