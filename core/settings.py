bbeba40caf989c6b7a7f81a7093cb35395a4695d
namanmistry <65106237+namanmistry@users.noreply.github.com>
2021-04-16 14:06:47 +0530

    final

--- core/settings.py
+++ core/settings.py
@@ -141,5 +141,5 @@ LOGIN_REDIRECT_URL= 'blog-home'
 
 LOGIN_URL='login'
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+# MEDIA_URL='/media/'
+# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
