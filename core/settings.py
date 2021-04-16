43f4a5399ef35b3149da7d951934259ab9a1e245
namanmistry <65106237+namanmistry@users.noreply.github.com>
2021-04-16 14:12:35 +0530

    final

--- core/settings.py
+++ core/settings.py
@@ -141,7 +141,7 @@ LOGIN_REDIRECT_URL= 'blog-home'
 
 LOGIN_URL='login'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
-# STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # MEDIA_URL='/media/'
 # MEDIA_ROOT=os.path.join(BASE_DIR,'media')
