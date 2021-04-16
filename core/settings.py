56a6529e5c084f46c5f7e701078d1d5a26c96a1e
namanmistry <65106237+namanmistry@users.noreply.github.com>
2021-04-16 14:09:57 +0530

    final

--- core/settings.py
+++ core/settings.py
@@ -140,6 +140,8 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL= 'blog-home'
 
 LOGIN_URL='login'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # MEDIA_URL='/media/'
 # MEDIA_ROOT=os.path.join(BASE_DIR,'media')
